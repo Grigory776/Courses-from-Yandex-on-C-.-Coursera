631e733dc9b4a554ddd6cb0650a480e54c5a3a44
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-08 04:54:03 +0800

    Add files via upload

---
+++ Перевод из 10 в 2 СС.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void perevod(int num);
+
+int main(){
+
+	
+	int n;
+	cin >> n;
+	cout << endl;
+	perevod(n);
+
+
+	return 0;
+}
+
+void perevod(int num) {
+
+	vector <int> d;
+
+	while ((num / 2) != 0) {
+		d.push_back(num % 2);
+		num /= 2;
+	}
+
+	if (num == 0) {
+		d.push_back(0);
+	}
+	else {
+		d.push_back(1);
+	}
+
+	for (int i = size(d) - 1; i >= 0;i--) {
+		cout << d[i];
+
+	}
+	cout << endl;
+}
