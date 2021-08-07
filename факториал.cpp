4858de4662de22759a0baa23dbd788c87d46a66a
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-08 05:10:29 +0800

    Add files via upload

---
+++ факториал.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n) {
+	int res=1;
+	while (n > 0) {
+		res = res * n;
+		n -= 1;
+	}
+	return res;
+}
+
+
+int main(){
+	cout << factorial(1) << endl << factorial(0) << endl << factorial(4) << endl;
+
+	return 0;
+}
