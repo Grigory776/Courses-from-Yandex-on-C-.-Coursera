f4c7a7bdcafd499fecf69b68a68f5efda01be631
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:37:08 +0800

    Add files via upload

---
+++ VS_Day6_DZ15_(obratn por vectora).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> Reversed(const vector<int>& v) {
+
+    vector<int> temp = v;
+    int p = v.size() - 1;
+    for (int i = 0; i < v.size() / 2; i++) {
+        swap(temp[i], temp[p]);
+        --p;
+    }
+    return temp;
+}
+
+int main()
+{
+    vector<int> numbers = {-14,36,4,8,19};
+    numbers = Reversed(numbers);
+
+    for (auto x : numbers)
+        cout << x << ' ';
+    cout << endl;
+
+
+    return 0;
+
+}
