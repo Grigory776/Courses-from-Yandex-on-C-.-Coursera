cb317d62475b9ac775a54582b7239c84daa01382
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:36:58 +0800

    Add files via upload

---
+++ VS_Day7_DZ16_(sr znach).cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N; // количество дней которые требуется рассмотреть
+
+	vector<int> tempDay(N);
+
+	for (int i = 0; i < N; i++)
+		cin >> tempDay[i]; // вводим температуру дней, начиная с 0-ого
+
+	int Sum = 0;
+	int SrAr;  // Среднее арифмитическое температуры;
+
+	for (int i = 0; i < N; i++)
+		Sum+= tempDay[i];
+
+	SrAr = Sum / N; // Средняя температура
+	Sum = 0;
+
+	for (int i = 0; i < N; ++i) {
+		if (tempDay[i] > SrAr) {
+			++Sum;
+		}
+	}
+	cout << Sum << endl;
+
+	for (int i = 0; i < N; ++i) {
+		if (tempDay[i] > SrAr) {
+			cout << i << ' ';
+		}
+	}
+	cout << endl;
+
+	return 0;
+}
+
+
