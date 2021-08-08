14dbb7977b03397bc7d8518b8e2feb5e7628a308
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:41:25 +0800

    Add files via upload

---
+++ VS_Day9_DZ24_(classi!!!).cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+class SortedStrings {
+public:
+    void AddString(const string& s) {
+        str.push_back(s);
+    }
+    vector<string> GetSortedStrings() {
+       sort(begin(str), end(str));
+       return str;
+    }
+private:
+    vector<string> str;
+};
+
+void PrintSortedStrings(SortedStrings& s) {
+    for (int i = 0; i < s.GetSortedStrings().size(); i++)
+        cout << s.GetSortedStrings()[i] << ' ';
+    cout << endl;
+}
+
+int main() {
+    SortedStrings strings;
+
+    strings.AddString("first");
+    strings.AddString("third");
+    strings.AddString("second");
+    PrintSortedStrings(strings);
+
+    strings.AddString("second");
+    PrintSortedStrings(strings);
+
+    return 0;
+}
