5095e1ea018388dcc4a4485fe2c2c920941801a9
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:34:55 +0800

    Add files via upload

---
+++ VS_Day4_DZ8_(NOD).cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+ /*
+    int a, b, temp=0;
+    cin >> a >> b;
+    vector<int> vect1, vect2;
+
+    for (int i = 1; i <= a; i++) {
+        if ((a % i) == 0)
+            vect1.push_back(i);
+   }
+
+    for (int i = 1; i <= b; i++) {
+        if ((b % i) == 0)
+            vect2.push_back(i);
+    }
+
+   
+
+    for (int i = 0; i < vect1.size(); i++) {
+        for (int j = 0; j < vect2.size(); j++) {
+            if (vect1[i] == vect2[j])
+                if (vect1[i] > temp)
+                    temp = vect1[i];
+        }
+    }
+    if (temp!=0)
+    cout << temp << endl;
+ */
+
+    int a, b;
+    cin >> a >> b;
+    while ((a>0)&&(b>0))
+    {
+        if (a > b)
+            a %= b;
+        else
+            b %= a;
+    }
+
+
+    cout << a + b << endl;
+
+    return 0;
+}
+
