1346df990e81dd0058c68612506e4f74bc289981
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:08:29 +0800

    Add files via upload

---
+++ класс отсортированных строк.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+class SortedStrings {
+public:
+	void AddString(const string& s) {
+		strings.push_back(s);
+		
+	}
+
+	vector <string> GetSortedStrings() {
+		sort(begin(strings), end(strings));
+		return strings;
+	}
+private:
+	vector<string> strings;
+
+};
+
+void PrintSortedStrings(SortedStrings& st) {
+	for (auto s : st.GetSortedStrings()) {
+		cout << s << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	SortedStrings strings;
+
+	strings.AddString("first");
+	strings.AddString("third");
+	strings.AddString("second");
+	PrintSortedStrings(strings);
+
+	strings.AddString("second");
+	PrintSortedStrings(strings);
+
+
+	return 0;
+}
