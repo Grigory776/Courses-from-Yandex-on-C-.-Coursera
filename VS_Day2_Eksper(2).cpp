5f4ab2efb44c92f97762f3251bc8c60311466a49
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:33:31 +0800

    Add files via upload

---
+++ VS_Day2_Eksper(2).cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world" <<endl;
+
+	string Hw = "Hello,world!";
+
+	for (char c : Hw)
+		cout << c << "  ";
+	cout << endl;
+
+	int arr[] = { 9, 2, -36, 44, 1, 15, 1 };
+
+	for (int c : arr)
+		cout << c << "  ";
+	cout << endl;
+
+	sort(begin(arr), end(arr));
+	
+	for (int c : arr)
+		cout << c << "  ";
+	cout << endl;
+
+	cout << count(begin(Hw), end(Hw), 'l') << endl;
+
+
+	return 0;
+}
