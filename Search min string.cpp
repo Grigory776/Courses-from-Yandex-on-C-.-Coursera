5685ba52d9982ff8391d61ff8e6f49ac63f9ed03
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:31:49 +0800

    Add files via upload

---
+++ Search min string.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    string b;
+    string c;
+
+    cin >> a >> b >> c;
+
+    if ((a != b) && (b != c) && (a != c)) {
+
+        if ((a < b) && (a < c))
+            cout << a << endl;
+        if ((b < a) && (b < c))
+            cout << b << endl;
+        if ((c < a) && (c < b))
+            cout << c << endl;
+    }
+
+    if ((a == b) && (b == c))
+        cout << a;
+
+    else {
+        if ((a == b)) {
+            if (a < c)
+                cout << a;
+            else
+                cout << c;
+        }
+        if ((a == c)) {
+            if (a < b)
+                cout << a;
+            else
+                cout << b;
+        }
+        if ((b == c)) {
+            if (a < c)
+                cout << a;
+            else
+                cout << c;
+        }
+       
+    }
+
+    return 0;
+}
+
+
