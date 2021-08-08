47056b955bb05b839f71475a93a7b3ac98ec5af7
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:36:08 +0800

    Add files via upload

---
+++ VS_Day6_DZ13_(link).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void UpdateIfGreater(int first, int& second) {
+
+    if (first > second)
+        second = first;
+}
+
+int main()
+{
+    int a, b;
+    a = 4;
+    b = 2;
+
+    UpdateIfGreater(a, b);
+
+    return 0;
+}
+
