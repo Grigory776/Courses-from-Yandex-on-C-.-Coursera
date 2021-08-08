e62e03fedb3c54e1e39824a45725e24711ed597b
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:35:22 +0800

    Add files via upload

---
+++ VS_Day6_DZ11_.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+bool IsPalindrom(string a) {
+
+    if (a.size() < 2)
+        return true;
+
+    for (int i = 0; i < a.size() / 2; i++)
+        if (a[i] != a[a.size() - 1 - i])
+            return false;
+    return true;
+    
+}
+
+int main()
+{
+    string x;
+    cin >> x;
+
+    cout << IsPalindrom(x) << endl;
+
+    return 0;
+}
+
