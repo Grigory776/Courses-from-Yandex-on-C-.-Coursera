2b5291aecf3711f3a7cd16c912a1c4b31af0cd09
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:34:17 +0800

    Add files via upload

---
+++ VS_Day3_DZ5_(rasschet stoimosti).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double N, A, B, X, Y;
+
+    cin >> N >> A >> B >> X >> Y;
+
+    if (N > B)
+        N = (N / 100) * (100 - Y);
+    else if (N > A)
+        N = (N / 100) * (100 - X);
+
+    cout << N << endl;
+    return 0;
+}
+
+
