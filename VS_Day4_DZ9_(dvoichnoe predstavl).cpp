f86cab9c55d77bd504e608b22945ad94d3876dce
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:35:06 +0800

    Add files via upload

---
+++ VS_Day4_DZ9_(dvoichnoe predstavl).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> VN;
+
+    do
+    {
+        VN.push_back(N % 2);
+        N /= 2;
+        if (N == 1)
+            VN.push_back(1);
+
+    } while (N / 2 != 0);
+
+    if (VN[VN.size() - 1] != 0) {
+        for (int i = VN.size() - 1; i >= 0; i--)
+            cout << VN[i];
+    }
+
+   
+
+    return 0;
+}
+
