7dba5580cbea30f85f487bf451be893e0510f48d
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:34:28 +0800

    Add files via upload

---
+++ VS_Day4_DZ_7_(2oe vxozhden).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string v;
+    int i=0,temp=0;
+    cin >> v;
+    for (char c : v) {
+        if (c == 'f')
+            temp += 1;
+        if (temp == 2) {
+            cout << i << endl;
+            temp += 1;
+        }
+        i += 1;
+    }
+    if (temp == 0)
+        cout << "-2" << endl;
+    else if (temp == 1)
+        cout << "-1" << endl;
+
+    return 0;
+}
+
