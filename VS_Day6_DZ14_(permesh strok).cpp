0a9888b615f527f13ffbac7b8ea277f1675c2831
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:36:22 +0800

    Add files via upload

---
+++ VS_Day6_DZ14_(permesh strok).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include<vector>
+
+using namespace std;
+
+void MoveStrings(vector<string>& source, vector<string>& destination) {
+    for (auto x : source) {
+        destination.push_back(x);
+    }
+    source.clear();
+}
+
+int main()
+{
+    vector<string> source = {"1","2", "4"};
+    vector<string> destination = { "z" };
+
+    MoveStrings(source, destination);
+    if (source.size() == 0)
+        cout << "true" << endl;;
+
+    for (auto x : destination)
+        cout << x << ' ' ;
+    cout << endl;
+
+    return 0;
+}
+
