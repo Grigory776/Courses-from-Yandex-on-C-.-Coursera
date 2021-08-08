1d96382ba0dc69c5b65946f7588c080c37d08f0c
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-08 22:26:33 +0800

    Add files via upload

---
+++ IsPalindrom.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+#include <iostream>
+
+using namespace std;
+
+bool IsPalindrom(const string t);
+
+
+int main() {
+
+	string sl;
+	cin >> sl;
+	cout << endl << IsPalindrom(sl);
+
+	return 0;
+}
+
+bool IsPalindrom(const string t) {
+
+	bool res = true;
+	int j = size(t) - 1;
+	for (size_t i = 0; i < size(t) / 2; i++) {
+		if (t[i] != t[j]) {
+			return false;
+		}
+		j--;
+	}
+	return true;
+
+}
