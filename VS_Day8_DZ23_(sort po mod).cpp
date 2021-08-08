f879099c0c629f5ff4e8c2d6eaef841097d56657
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:41:05 +0800

    Add files via upload

---
+++ VS_Day8_DZ23_(sort po mod).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool norma(int i, int j) { //abs() - функция модуля
+    return (abs(i) < abs(j));
+}
+void Print(const vector<int>& v) {
+    for (const auto& i : v)
+        cout << i << ' ';
+    
+}
+
+int main()
+{
+   
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    
+    for (int i = 0; i < N; ++i) 
+        cin >> arr[i];
+    
+    sort(begin(arr), end(arr), norma);
+    Print(arr);
+
+
+    return 0;
+}
+
+
