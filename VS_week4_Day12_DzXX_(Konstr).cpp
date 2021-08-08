4a914c64d9b55a81422f1a549e7cbe19ae1f2207
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-09 05:43:05 +0800

    Add files via upload

---
+++ VS_week4_Day12_DzXX_(Konstr).cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+struct Specialization {
+    string val;
+    explicit Specialization(string newv) {
+        val = newv;
+    }
+
+};
+struct Course {
+    string val;
+    explicit Course(string newv) {
+        val = newv;
+    }
+};
+struct Week {
+    string val;
+    explicit Week(string newv) {
+        val = newv;
+    }
+
+};
+
+struct LectureTitle {
+    string specialization;
+    string course;
+    string week;
+
+    LectureTitle(Specialization a, Course b, Week c) {
+        specialization = a.val;
+        course = b.val;
+        week = c.val;
+    }
+};
+
+int main()
+{
+    LectureTitle title(
+        Specialization("C++"),
+        Course("White belt"),
+        Week("4th")
+    );
+
+    return 0;
+}
+
